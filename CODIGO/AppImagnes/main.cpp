7159a2909ac0eab268e5372a644342e19b1372e8
29022020 <juanfml07@hotmail.com>
2021-09-17 23:49:51 -0500

    Se añade a la caperta un código auxiliar.
    Además de ciertas modififaciones en el código de interacción, se añade un código auxiliar.

--- CODIGO/AppImagnes/main.cpp
+++ CODIGO/AppImagnes/main.cpp
@@ -5,24 +5,31 @@ using namespace std;
 
 int main()
 {
-    string filename="../AppImagnes/Imagenes/RM.jpg";
+    string filename="RM.jpg";
+
 
     QImage im(filename.c_str());
 
     unsigned int pixelX=898;
     unsigned int pixelY=776;
 
+    //QSize sizeImg=im.size();
+
+    //cout << "La imagen tiene un tamano de: "<< sizeImg.<<" * "<<sizeImg.width()<<endl;
+
     cout << "Valor en RED del pixel selccionado: "<<im.pixelColor(pixelX, pixelY).red()<<endl;
     cout << "Valor en BLUE del pixel selccionado: "<<im.pixelColor(pixelX, pixelY).blue()<<endl;
     cout << "Valor en GREEN del pixel selccionado: "<<im.pixelColor(pixelX, pixelY).green()<<endl;
 
-    for(int indx=0; indx<im.width(); ++indx){
+    cout << "The size of the image is: "<<im.width()<<" | "<<im.height()<<endl;
+
+    /*for(int indx=0; indx<im.width(); ++indx){
 
         for(int indy=0; indy <im.height(); ++indy){
 
             cout << "Valor en RED del pixel selccionado: "<<im.pixelColor(indx, indy).red()<<endl;
         }
-    }
+    }*/
 
     return 0;
 }
