662830cf9295ab7c27456d7bee17f7609d8da67d
29022020 <juanfml07@hotmail.com>
2021-09-18 21:47:29 -0500

    Version Estable de la prueba QImage.
    Primera version estable.
    Se modifican la función que escribe los archivos que contiene a la imagen con sus valor en RGB, un archivo para cada valor.

--- CODIGO/AppImagnes/main.cpp
+++ CODIGO/AppImagnes/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void CreateTxt(QImage *im, string *RedFile_, string *BlueFile_, string *GreenFile_);
+void CreateTxt(QImage *im, string RedFile_, string BlueFile_, string GreenFile_);
 
 string CharToString(char array[]);
 
@@ -31,133 +31,47 @@ int main()
     string BlueFile="";
     string GreemFile="";
 
-    CreateTxt(&im, &RedFile, &BlueFile, &GreemFile);
+    CreateTxt(&im, RedFile, BlueFile, GreemFile);
 
     return 0;
 
 }
 
-void CreateTxt(QImage *im, string *RedFile_, string *BlueFile_, string *GreenFile_){
+void CreateTxt(QImage *im, string RedFile_, string BlueFile_, string GreenFile_){
 
-    for(int indx=0; indx<(*im).width(); ++indx){
+    for(int indx=0; indx<(*im).width(); indx++){
 
-        for(int indy=0; indy <(*im).height(); ++indy){
+        for(int indy=0; indy <(*im).height(); indy++){
 
-            if(indy==0){
+            if(indy!=((*im).height()-1)){
 
-                (*RedFile_)+='-';
-                (*BlueFile_)+='-';
-                (*GreenFile_)+='-';
-
-                (*RedFile_)+=(*im).pixelColor(indx, indy).red();
-                (*BlueFile_)+=(*im).pixelColor(indx, indy).blue();
-                (*GreenFile_)+=(*im).pixelColor(indx, indy).green();
+                RedFile_+=to_string((*im).pixelColor(indx, indy).red())+'-';
+                BlueFile_+=to_string((*im).pixelColor(indx, indy).blue())+'-';
+                GreenFile_+=to_string((*im).pixelColor(indx, indy).green())+'-';
 
             }
             else{
 
-                if(indy!=(*im).height()){
-
-                (*RedFile_)+=(*im).pixelColor(indx, indy).red();
-                (*BlueFile_)+=(*im).pixelColor(indx, indy).blue();
-                (*GreenFile_)+=(*im).pixelColor(indx, indy).green();
-
-                (*RedFile_)+='-';
-                (*BlueFile_)+='-';
-                (*GreenFile_)+='-';
-
-                }
-                else{
-
-                    (*RedFile_)+=(*im).pixelColor(indx, indy).red();
-                    (*BlueFile_)+=(*im).pixelColor(indx, indy).blue();
-                    (*GreenFile_)+=(*im).pixelColor(indx, indy).green();
-
-                }
+                RedFile_+=to_string((*im).pixelColor(indx, indy).red());
+                BlueFile_+=to_string((*im).pixelColor(indx, indy).blue());
+                GreenFile_+=to_string((*im).pixelColor(indx, indy).green());
 
             }
 
         }
+        if(indx!=(*im).width()-1){
 
-        (*RedFile_)+='\n';
-        (*BlueFile_)+='\n';
-        (*GreenFile_)+='\n';
+        RedFile_+='\n';
+        BlueFile_+='\n';
+        GreenFile_+='\n';
+        }
 
     }
 
-    escribir("Red_file.txt", *RedFile_);
-    escribir("Blue_file.txt", *BlueFile_);
-    escribir("Red_Green.txt", *GreenFile_);
+    escribir("Red_file.txt", RedFile_);
+    escribir("Blue_file.txt", BlueFile_);
+    escribir("Green_file.txt", GreenFile_);
 
     cout << endl<<"Job Done"<<endl;
 
-}
-string CharToString(char array[])
-{
-    /*stringToInt: Función que una vez le ingreses un string que es una cadena de numeros, los convierte en enteros.
-     *
-     * Parametro: string text.
-     *
-     * Retorno: El valor entero del string ingresado.
-     *
-     */
-
-    string Elemento="";
-
-    for(int i=0; i<3; i++){
-
-        if(array[i]=='0'){
-
-            Elemento+="0";
-        }
-
-        if(array[i]=='1'){
-
-            Elemento+="1";
-
-        }
-        if(array[i]=='2'){
-
-            Elemento+="2";
-
-        }
-        if(array[i]=='3'){
-
-            Elemento+="3";
-
-        }
-        if(array[i]=='4'){
-
-            Elemento+="4";
-
-        }
-        if(array[i]=='5'){
-
-            Elemento+="5";
-        }
-        if(array[i]=='6'){
-
-            Elemento+="6";
-
-        }
-
-        if(array[i]=='7'){
-
-            Elemento+="7";
-
-        }
-        if(array[i]=='8'){
-
-            Elemento+="8";
-
-        }
-        if(array[i]=='9'){
-
-            Elemento+="9";
-
-        }
-
-    }
-
-    return Elemento;
 }
